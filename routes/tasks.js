163230cadb227fba6edba843646e3865682215c6
Marc0-03 <marcusnylund44@gmail.com>
2022-02-28 11:07:13 +0100

    ✨ Jobbat

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const pool = require('../database');
@@ -41,11 +42,37 @@ router.get('/new', (req, res, next) => {
 });
 
 /* POST a new task */
-router.post('/',
-  async (req, res, next) => {
-    const sql = 'INSERT INTO tasks (task) VALUES (?)';
-    const result = await pool.promise()
-    .query(sql, [req.body.task])
+router.post('/', async (req, res, next) => {
+    const sql = 'INSERT INTO tasks (task,taskInfo) VALUES (?,?)'; 
+    const task = req.body.task;
+    const taskInfo = req.body.taskInfo
+    if (task.length < 3) {
+        res.status(400).json({
+            tasks: {
+                error: "Invalid task"
+            }
+        });
+    } else if (taskInfo.length < 3) {
+        res.status(400).json({
+            tasks: {
+                error: "Invalid task"
+            }
+        });
+    }
+    await pool.promise()
+    .query(sql, [task, taskInfo])
+    .then((response) => {
+        console.log(response[0].affectedRows);
+        if (response[0].affectedRows==1) {
+        res.redirect('/tasks');
+        } else {
+            res.status(400).json({
+                tasks: {
+                    error: "Invalid task"
+                }
+            });
+        }
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json({
@@ -54,7 +81,6 @@ router.post('/',
             }
         });
     });
-    res.redirect('/tasks');
 });
 
 router.get('/:id', async (req, res, next) => {
@@ -89,12 +115,26 @@ router.get('/:id', async (req, res, next) => {
 
 router.get('/:id/delete', async (req, res, next) => {
     const id = req.params.id;
-    
-
     await pool.promise()
         .query('DELETE from tasks WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-            res.json(`deleting task ${id}`)
+        .then(() => {
+            res.redirect('/tasks');
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
+        })
+})
+router.get('/:id/complete', async (req, res, next) => {
+    const id = req.params.id;
+    await pool.promise()
+        .query('update tasks SET completed = 1, updated_at=now() where id = ?', [id])
+        .then(() => {
+            res.redirect('/tasks');
         })
         .catch(err => {
             console.log(err)
@@ -132,5 +172,7 @@ router.post('/:id/update', async (req, res, next) => {
 
 
 })
+
+
 
 module.exports = router;
